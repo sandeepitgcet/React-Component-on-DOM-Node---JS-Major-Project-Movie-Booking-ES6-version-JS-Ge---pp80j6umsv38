06bb8bf4fe6ff7281132e8049338a872a0c48f94
sandeepitgcet <sandeepitgcet@gmail.com>
2023-01-03 21:55:56 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function App() {
-  return (
-    <div>App</div>
-  )
+export default class App extends Component {
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
 }
+
