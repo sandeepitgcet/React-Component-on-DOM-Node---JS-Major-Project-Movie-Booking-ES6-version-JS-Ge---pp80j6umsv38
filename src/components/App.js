c141403b0d5679a42459e008861ee1643de69303
sandeepitgcet <sandeepitgcet@gmail.com>
2023-01-03 21:58:07 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>App</div>
-    )
-  }
+export default function App() {
+  return (
+    <>
+        
+    </>
+  )
 }
-
